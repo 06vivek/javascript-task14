d51e4cc3d0d3ae155b70d01d92cb7805f2fc7aa7
vivek <vivekpatidar3839@gmail.com>
2024-11-27 10:40:33 +0530

    adding files

---
+++ task13.1.js
@@ -0,0 +1,22 @@
+// Create a function that returns array of employees from a random time from 
+//1 to 2 seconds using async await.
+function employee1(){
+    return new Promise((resolve,reject)=>{
+      setTimeout(() => {
+          resolve(["tom","jerry","jack"])
+      }, 1000);
+    })
+}
+function employee2(){
+    return new Promise((resolve,reject)=>{
+      setTimeout(() => {
+          resolve(["hulk","spiderman","ironman","captan america"])
+      },1000);
+    })
+}
+
+async function result(){
+   await  employee1().then((res)=>{console.log(res)})
+ await  employee2().then((res)=>{console.log(res)})
+}
+result();
